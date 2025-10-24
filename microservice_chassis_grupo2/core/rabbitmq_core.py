182671fa1e909b1ac24bdc8919e0db575acd6ecc
julen <julen.gabilondo@alumni.mondragon.edu>
2025-10-24 16:58:12 +0200

    v5 con PUBLIC_KEY_PATH

--- microservice_chassis_grupo2/core/rabbitmq_core.py
+++ microservice_chassis_grupo2/core/rabbitmq_core.py
@@ -1,6 +1,8 @@
 from aio_pika import connect_robust, ExchangeType
 from microservice_chassis_grupo2.core.config import settings
 
+PUBLIC_KEY_PATH = "/home/pyuser/code/auth_public.pem"
+
 async def get_channel():
     connection = await connect_robust(settings.RABBITMQ_HOST)
     channel = await connection.channel()
