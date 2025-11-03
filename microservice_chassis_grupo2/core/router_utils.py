7e511a97ae55a46d0ec9ee25ec8ab8bf5b601cfd
julen <julen.gabilondo@alumni.mondragon.edu>
2025-11-03 16:49:30 +0100

    nombre de rutas para debugear con http

--- microservice_chassis_grupo2/core/router_utils.py
+++ microservice_chassis_grupo2/core/router_utils.py
@@ -4,11 +4,11 @@ import logging
 import os
 from fastapi import HTTPException
 
-ORDER_SERVICE_URL = f"https://{os.getenv("ORDER_SERVICE", "locahost")}:5000"
-MACHINE_SERVICE_URL = f"https://{os.getenv("MACHINE_SERVICE", "locahost")}:5001"
-DELIVERY_SERVICE_URL = f"https://{os.getenv("DELIVERY_SERVICE", "locahost")}:5002"
-PAYMENT_SERVICE_URL = f"https://{os.getenv("PAYMENT_SERVICE", "locahost")}:5003"
-AUTH_SERVICE_URL = f"https://{os.getenv("AUTH_SERVICE", "locahost")}:5004"
+ORDER_SERVICE_URL = f"{os.getenv("ORDER_SERVICE", "http://locahost")}:5000"
+MACHINE_SERVICE_URL = f"{os.getenv("MACHINE_SERVICE", "http://locahost")}:5001"
+DELIVERY_SERVICE_URL = f"{os.getenv("DELIVERY_SERVICE", "http://locahost")}:5002"
+PAYMENT_SERVICE_URL = f"{os.getenv("PAYMENT_SERVICE", "http://locahost")}:5003"
+AUTH_SERVICE_URL = f"{os.getenv("AUTH_SERVICE", "http://locahost")}:5004"
 
 logger = logging.getLogger(__name__)
 
