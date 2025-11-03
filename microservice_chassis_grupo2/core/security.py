ea8a5bebf5c0b8d61f9fb65a1300db77be823777
julen <julen.gabilondo@alumni.mondragon.edu>
2025-11-03 17:24:57 +0100

    nombre de rutas para debugear con http y localhost y publicpem

--- microservice_chassis_grupo2/core/security.py
+++ microservice_chassis_grupo2/core/security.py
@@ -3,7 +3,8 @@ import os
 from fastapi import HTTPException
 from microservice_chassis_grupo2.core.config import settings
 
-PUBLIC_KEY_PATH = "/home/pyuser/code/auth_public.pem"
+#"/home/pyuser/code/auth_public.pem"
+PUBLIC_KEY_PATH = os.getenv("PUBLIC_KEY_PATH", "auth_public.pem")
 
 def read_public_pem():
     if not os.path.exists(PUBLIC_KEY_PATH):
