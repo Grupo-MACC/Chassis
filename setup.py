a71cdda20dd31f93af92612c3b06f36d5d8ac57d
julen <julen.gabilondo@alumni.mondragon.edu>
2025-12-13 15:13:02 +0100

    consul client

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice_chassis_grupo2",
-    version="0.1.19",
+    version="0.1.20",
     packages=find_packages(),
     install_requires=[],
     author="Grupo 2",
