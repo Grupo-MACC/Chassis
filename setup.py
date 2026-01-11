cadea779558aec0801a6518c367ab256b92bdaad
gorkapereza-lab <gorka.pereza@alumni.mondragon.edu>
2026-01-11 13:00:03 +0100

    Bump version to 0.1.5rc2 (TLS RabbitMQ)

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="microservice_chassis_grupo2_cc",
-    version="0.1.5rc1",
+    name="microservice_chassis_grupo2",
+    version="0.1.5rc2",
     packages=find_packages(),
     install_requires=[],
     author="Grupo 2",
