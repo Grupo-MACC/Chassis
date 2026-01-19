d2abc30a088f0c86ec89555695eba565cf24a0fd
julen <julen.gabilondo@alumni.mondragon.edu>
2026-01-19 16:08:25 +0100

    channel 1.4

--- microservice_chassis_grupo2/core/consul.py
+++ microservice_chassis_grupo2/core/consul.py
@@ -34,6 +34,7 @@ class ConsulClient:
                 response = await client.get(
                     f"{self.base_url}/catalog/service/{service_name}",
                     timeout=10.0,
+                    verify=False
                 )
                 if response.status_code == 200:
                     services = response.json()
