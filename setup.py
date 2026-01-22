749fde054baf25253c8557d320fb86cd667167f2
julen <julen.gabilondo@alumni.mondragon.edu>
2026-01-22 15:07:34 +0100

    1.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="microservice_chassis_grupo2_cc_prod_secrets",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
     use_scm_version=True,  # <-- clave: versión por tags
     description="A reusable library for microservices",
